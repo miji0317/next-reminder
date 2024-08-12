e3a130fea3d3ab99783d89897d503f5e734c623f
miji <mijiunknown@gmail.com>
2024-08-12 23:55:26 +0900

    Fix: 모달 레이어 백그라운드 클릭 액션 수정

--- src/app/_component/ModalLayout.tsx
+++ src/app/_component/ModalLayout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styled from 'styled-components';
-import { ReactNode } from 'react';
+import { MouseEvent, ReactNode } from 'react';
 import { useRouter } from 'next/navigation';
 import { colors } from '@/styles/colors';
 import { media, breakPoints } from '@/styles/media';
@@ -10,19 +10,28 @@ type Props = { children: ReactNode };
 
 export default function ModalLayout({ children }: Props) {
   const router = useRouter();
-  const closeModal = () => {
-    router.back(); // 뒤로가기
+
+  // 모달 외 백그라운드 영역 클릭시 모달 닫기
+  const closeModal = (event: MouseEvent<HTMLDivElement>) => {
+    if (event.currentTarget.id === 'modalBackLayer') {
+      router.back(); // 뒤로가기
+    }
+  };
+
+  // 모달 영역 클릭시 닫힘 방지
+  const preventClick = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
   };
 
   return (
-    <BackLayer onClick={closeModal}>
-      <ModalLayer>{children}</ModalLayer>
+    <BackLayer id="modalBackLayer" onClick={closeModal}>
+      <ModalLayer onClick={preventClick}>{children}</ModalLayer>
     </BackLayer>
   );
 }
 
 const BackLayer = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
   min-width: 100%;
